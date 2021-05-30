098e246ba0a121da0a498dd37c0623630dca746f
igoride <45434626+igoride@users.noreply.github.com>
2021-05-30 19:48:43 -0300

    Update pi_Montecarlo.c
    so n=10 q n funfa

--- Projeto1/pi_Montecarlo.c
+++ Projeto1/pi_Montecarlo.c
@@ -7,14 +7,15 @@ int main(){
     srand(time(NULL));
     
     int i;
-    float  count=0, N = 100000;
+    double  count=0; 
+	long double N = 3100000000;
     double d, x, y, pi, tempo;
     clock_t t;
-    
+	    
     t = clock();
     
     for(i=0;i<N;i++){
-        
+            
         x = ((double) rand() / (RAND_MAX));
         y = ((double) rand() / (RAND_MAX));
         
@@ -26,15 +27,16 @@ int main(){
         if(d<1){
             count++;
         }
+           
     }
-    
+        
     pi = 4*(count/N);
-    
+		 
     t = clock() - t;
     tempo = ((double) t)/CLOCKS_PER_SEC;
-    
-    printf("%lf \n", tempo);
-    printf("\n%lf = 4 * (%f / %f)", pi, count, N);
-    
+        
+    printf("tempo = %lf \n", tempo);
+    printf("\n%lf = 4 * (%f / %f)\n", pi, count, N);
+        
     return 0;
 }
